5096a56ca1b3758b5bea69b99ec4f91b4fb30db2
Pranshu1902 <pranshu1902@gmail.com>
2022-09-30 12:15:03 +0530

    debug

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Started express server at port 3000");
 });
